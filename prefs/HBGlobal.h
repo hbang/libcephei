02251ee37a59a4f1748ffbd86761369db508cfe3
insanj <insanj@gmail.com>
2014-04-06 16:05:21 -0400

    minor Makefile fix, comments and -cachedTintColor in HBListController, added .theos to .gitignore

--- prefs/HBGlobal.h
+++ prefs/HBGlobal.h
@@ -1,1 +1,4 @@
-../HBGlobal.h
+NSBundle *globalBundle;
+
+#define L18N(key) [globalBundle localizedStringForKey:key value:key table:@"libhbangprefs"]
+#define URL_ENCODE(string) [(NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)(string), NULL, CFSTR(":/=,!$& '()*+;[]@#?"), kCFStringEncodingUTF8) autorelease]
--- prefs/HBListController.m
+++ prefs/HBListController.m
@@ -5,7 +5,6 @@
 @class HBRootListController;
 
 #define IS_MODERN IS_IOS_OR_NEWER(iOS_7_0)
-;
 
 @interface HBListController () {
 	UIColor *_cachedTintColor;
@@ -23,45 +22,53 @@
 
 #pragma mark - UIViewController
 
+// TODO: figure out why none of these methods (that's right, none) are called when
+// going back into Preferences after exiting.
+
 - (void)viewDidLoad {
+	// I'm not even gonna ask what this is about... https://www.youtube.com/watch?v=BkWl679wB1c
+	[self cachedTintColor];
 	[super viewDidLoad];
+}
 
+// If needed, and possible, runs through every view controller on the current navigation
+// stack, pulling the Hashbang tintColor from the root Hashbang list view controller.
+- (UIColor *)cachedTintColor {
 	if (IS_MODERN) {
-		// https://www.youtube.com/watch?v=BkWl679wB1c
-		UIColor *tintColor = [self.class hb_tintColor];
-		NSArray *viewControllers = self.navigationController.viewControllers;
+		if (!_cachedTintColor) {
+			NSArray *viewControllers = self.navigationController.viewControllers;
+			UIColor *tintColor = [self.class hb_tintColor];
 
-		if (!tintColor) {
-			NSInteger i = viewControllers.count;
+			if (!tintColor) {
+				NSInteger i = viewControllers.count;
 
-			while (--i) {
-				if ([((NSObject *)viewControllers[i]).class respondsToSelector:@selector(hb_tintColor)] && [((HBListController *)viewControllers[i]).class hb_tintColor]) {
-					tintColor = [((HBListController *)viewControllers[i]).class hb_tintColor];
-					break;
+				while (--i) {
+					if ([((NSObject *)viewControllers[i]).class respondsToSelector:@selector(hb_tintColor)] && [((HBListController *)viewControllers[i]).class hb_tintColor]) {
+						tintColor = [((HBListController *)viewControllers[i]).class hb_tintColor];
+						break;
+					}
 				}
 			}
+
+			_cachedTintColor = [tintColor copy];
 		}
 
-		_cachedTintColor = [tintColor copy];
-
-		[UISwitch appearanceWhenContainedIn:self.class, nil].onTintColor = tintColor;
-		[UILabel appearanceWhenContainedIn:HBTintedTableCell.class, nil].textColor = tintColor;
+		return _cachedTintColor;
 	}
+
+	return nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-	[super viewWillAppear:animated];
-
-	UITableView *tableView = [self respondsToSelector:@selector(table)] ? self.table : self.view;
-
-	// fix weird bug where selected row doesn't deselect
-	// thanks insanj <4
-	[tableView deselectRowAtIndexPath:tableView.indexPathForSelectedRow animated:YES];
-
 	if (IS_MODERN) {
-		self.view.tintColor = _cachedTintColor;
-		self.navigationController.navigationBar.tintColor = _cachedTintColor;
+		self.view.tintColor = [self cachedTintColor];
+		self.navigationController.navigationBar.tintColor = [self cachedTintColor];
+
+		[UISwitch appearanceWhenContainedIn:self.class, nil].onTintColor = [self cachedTintColor];
+		[UILabel appearanceWhenContainedIn:HBTintedTableCell.class, nil].textColor = [self cachedTintColor];
 	}
+
+	[super viewWillAppear:animated];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -71,6 +78,13 @@
 		self.view.tintColor = nil;
 		self.navigationController.navigationBar.tintColor = nil;
 	}
+}
+
+// Fixes weird iOS 7 glitch, a little neater than before, and ideally
+// preventing crashes on iPads and older devices.
+- (void)tableView:(id)arg1 didSelectRowAtIndexPath:(id)arg2 {
+	[super tableView:arg1 didSelectRowAtIndexPath:arg2];
+	[arg1 deselectRowAtIndexPath:arg2 animated:YES];
 }
 
 @end
