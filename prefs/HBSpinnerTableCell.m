6689ca258192d8b7f11f659d0e9b864b4f93eea4
Adam D <kirbylover4000@gmail.com>
2014-12-14 16:33:04 +1030

    add HBSpinnerTableCell from dailypaper

---
+++ prefs/HBSpinnerTableCell.m
@@ -0,0 +1,35 @@
+#import "HBSpinnerTableCell.h"
+
+@implementation HBSpinnerTableCell {
+	UIActivityIndicatorView *_activityIndicator;
+}
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(id)specifier {
+	self = [super initWithStyle:style reuseIdentifier:reuseIdentifier specifier:specifier];
+
+	if (self) {
+		_activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+		self.accessoryView = _activityIndicator;
+	}
+
+	return self;
+}
+
+- (void)setCellEnabled:(BOOL)cellEnabled {
+	[super setCellEnabled:cellEnabled];
+
+	if (cellEnabled) {
+		[_activityIndicator stopAnimating];
+	} else {
+		[_activityIndicator startAnimating];
+	}
+}
+
+#pragma mark - Memory management
+
+- (void)dealloc {
+	[_activityIndicator release];
+	[super dealloc];
+}
+
+@end
