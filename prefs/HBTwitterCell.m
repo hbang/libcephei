3eacc7b50f7b821c855ad4fda7204e665445ed92
Adam D <kirbylover4000@gmail.com>
2015-01-08 04:03:29 +1030

    implement presenting an avatar alongside twitter cells

--- prefs/HBTwitterCell.m
+++ prefs/HBTwitterCell.m
@@ -8,6 +8,11 @@
 	NSString *_user;
 	UIImage *_defaultImage;
 	UIImage *_highlightedImage;
+
+	UIView *_avatarView;
+	UIImageView *_avatarImageView;
+
+	BOOL _isBig;
 }
 
 @end
@@ -15,11 +20,12 @@
 @implementation HBTwitterCell
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier {
-	self = [super initWithStyle:specifier.properties[@"big"] && ((NSNumber *)specifier.properties[@"big"]).boolValue ? UITableViewCellStyleSubtitle : UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier specifier:specifier];
+	BOOL isBig = specifier.properties[@"big"] && ((NSNumber *)specifier.properties[@"big"]).boolValue;
+	self = [super initWithStyle:isBig ? UITableViewCellStyleSubtitle : UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier specifier:specifier];
 
 	if (self) {
+		_isBig = isBig;
 		_user = [specifier.properties[@"user"] copy];
-
 		_defaultImage = [[UIImage imageNamed:@"twitter" inBundle:globalBundle] retain];
 
 		if (!IS_MODERN) {
@@ -30,9 +36,48 @@
 		self.detailTextLabel.textColor = IS_MODERN ? [UIColor colorWithWhite:0.5568627451f alpha:1] : [UIColor tableCellValue1BlueColor];
 		self.selectionStyle = UITableViewCellSelectionStyleBlue;
 		self.accessoryView = [[UIImageView alloc] initWithImage:_defaultImage];
+
+		if (!specifier.properties[@"showAvatar"] || ((NSNumber *)specifier.properties[@"showAvatar"]).boolValue) {
+			CGFloat size = _isBig ? 38.f : 29.f;
+
+			_avatarView = [[UIView alloc] initWithFrame:CGRectMake(IS_MODERN ? 15.f : 8.f, 0, size, size)];
+			_avatarView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+			_avatarView.center = CGPointMake(_avatarView.center.x, self.contentView.frame.size.height / 2);
+			_avatarView.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1];
+			_avatarView.userInteractionEnabled = NO;
+			_avatarView.clipsToBounds = YES;
+			_avatarView.layer.cornerRadius = IS_MODERN ? 4.f : size / 2;
+			[self.contentView addSubview:_avatarView];
+
+			_avatarImageView = [[UIImageView alloc] initWithFrame:_avatarView.bounds];
+			_avatarImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+			_avatarImageView.alpha = 0;
+			_avatarImageView.userInteractionEnabled = NO;
+			[_avatarView addSubview:_avatarImageView];
+
+			[self loadAvatarIfNeeded];
+		}
 	}
 
 	return self;
+}
+
+- (void)layoutSubviews {
+	[super layoutSubviews];
+
+	if (_avatarView) {
+		CGFloat extra = _avatarView.frame.size.width + 8.f;
+
+		CGRect labelFrame = self.textLabel.frame;
+		labelFrame.origin.x += extra;
+		self.textLabel.frame = labelFrame;
+
+		if (_isBig) {
+			CGRect detailFrame = self.detailTextLabel.frame;
+			detailFrame.origin.x += extra;
+			self.detailTextLabel.frame = detailFrame;
+		}
+	}
 }
 
 - (void)setSelectionStyle:(UITableViewCellSelectionStyle)style {
@@ -68,10 +113,40 @@
 	}
 }
 
+#pragma mark - Avatar
+
+- (void)loadAvatarIfNeeded {
+	if (!_avatarView || _avatarImageView.image) {
+		return;
+	}
+
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+		NSError *error = nil;
+		NSData *data = [NSURLConnection sendSynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://twitter.com/%@/profile_image?size=bigger", URL_ENCODE(_user)]]] returningResponse:nil error:&error];
+
+		if (error) {
+			NSLog(@"libcephei: error loading twitter avatar: %@", error);
+			return;
+		}
+
+		dispatch_async(dispatch_get_main_queue(), ^{
+			_avatarImageView.image = [UIImage imageWithData:data];
+
+			[UIView animateWithDuration:0.15 animations:^{
+				_avatarImageView.alpha = 1;
+			}];
+		});
+	});
+}
+
+#pragma mark - Memory management
+
 - (void)dealloc {
 	[_user release];
 	[_defaultImage release];
 	[_highlightedImage release];
+	[_avatarView release];
+	[_avatarImageView release];
 
 	[super dealloc];
 }
