05a63b20df656404337c1960e117f2222e0f1b88
Adam Demasi <adam@hbang.com.au>
2015-12-12 18:31:01 +1030

    [CompactConstraint] add documentation headers

--- CompactConstraint/NSLayoutConstraint+CompactConstraint.h
+++ CompactConstraint/NSLayoutConstraint+CompactConstraint.h
@@ -3,6 +3,21 @@
 //  Copyright (c) 2014 Marco Arment. See included LICENSE file.
 //
 
+/**
+ * NSLayoutConstraint (CompactConstraint), a class category from Marco Arment’s
+ * [CompactConstraint](https://github.com/marcoarment/CompactConstraint)
+ * library, is integrated with Cephei. CompactConstraint provides an Auto Layout
+ * grammar and methods that are easier to use and understand than UIKit’s built
+ * in functions for programmatically adding constraints.
+ *
+ * Refer to
+ * [its readme](https://github.com/marcoarment/CompactConstraint/blob/master/README.md)
+ * to learn how to use it. There are two changes to note: most importantly, the
+ * methods have an `hb_` prefix, and two methods that are marked as deprecated
+ * in the original project have been removed.
+ *
+ * CompactConstraint is licensed under the MIT License.
+ */
 @interface NSLayoutConstraint (CompactConstraint)
 
 + (instancetype)hb_compactConstraint:(NSString *)relationship metrics:(NSDictionary *)metrics views:(NSDictionary *)views self:(id)selfView;
