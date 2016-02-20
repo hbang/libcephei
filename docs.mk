7d4ad74920bf7b69a50c70093beb5ff8b76bfa7f
Adam Demasi <adam@hbang.com.au>
2016-02-20 14:59:18 +1030

    [docs] this broke docs generation. i should really learn how to make

--- docs.mk
+++ docs.mk
@@ -9,7 +9,7 @@ DOCS_OUTPUT_PATH = docs
 
 all:: docs
 
-docs: $(APPLEDOCFILES)
+docs:
 	# eventually, this should probably be in theos.
 	# for now, this is good enough :p
 
@@ -20,7 +20,7 @@ docs: $(APPLEDOCFILES)
 		--docset-atom-filename xcode-docset.atom --docset-package-url "https://hbang.github.io/libcephei/docset.xar" \
 		--docset-package-filename docset --docset-fallback-url "https://hbang.github.io/libcephei/" --docset-feed-name Cephei \
 		--index-desc README.md --no-repeat-first-par --include info \
-		--output "$(DOCS_STAGING_DIR)" $<
+		--output "$(DOCS_STAGING_DIR)" $(APPLEDOCFILES)
 
 	[[ -d "$(DOCS_OUTPUT_PATH)" ]] || git clone -b gh-pages git@github.com:hbang/libcephei.git "$(DOCS_OUTPUT_PATH)"
 	rsync -ra "$(DOCS_STAGING_DIR)"/{html,publish}/ "$(DOCS_OUTPUT_PATH)"
