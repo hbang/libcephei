a7d37d96b70e478d6fb62319706f7203bc570fb9
Adam Demasi <adam@hbang.com.au>
2016-09-05 23:19:26 +0930

    [HBLinkTableCell,HBTwitterCell] make twitter cell a link cell subclass

--- prefs/HBLinkTableCell.m
+++ prefs/HBLinkTableCell.m
@@ -1,21 +1,31 @@
 #import "HBLinkTableCell.h"
 #import <Preferences/PSSpecifier.h>
+#import <UIKit/UIColor+Private.h>
 #import <UIKit/UIImage+Private.h>
+#import <version.h>
 
-@implementation HBLinkTableCell
+@implementation HBLinkTableCell {
+	UIImageView *_avatarImageView;
+}
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier {
-	self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier specifier:specifier];
+	// if we are big, we need to know right now so we can set the style
+	BOOL isBig = specifier.properties[@"big"] && ((NSNumber *)specifier.properties[@"big"]).boolValue;
+
+	self = [super initWithStyle:isBig ? UITableViewCellStyleSubtitle : UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier specifier:specifier];
 
 	if (self) {
+		_isBig = isBig;
+
 		self.selectionStyle = UITableViewCellSelectionStyleBlue;
 		self.accessoryView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"safari" inBundle:globalBundle]];
 
-		self.detailTextLabel.numberOfLines = 0;
+		self.detailTextLabel.numberOfLines = isBig ? 0 : 1;
 		self.detailTextLabel.text = specifier.properties[@"subtitle"] ?: @"";
+		self.detailTextLabel.textColor = IS_MODERN ? [UIColor colorWithWhite:142.f / 255.f alpha:1] : [UIColor tableCellValue1BlueColor];
 
-		if (specifier.properties[@"initials"]) {
-			CGFloat size = 29.f;
+		if (self._shouldShowAvatar) {
+			CGFloat size = isBig ? 38.f : 29.f;
 
 			UIGraphicsBeginImageContextWithOptions(CGSizeMake(size, size), NO, [UIScreen mainScreen].scale);
 			specifier.properties[@"iconImage"] = UIGraphicsGetImageFromCurrentImageContext();
@@ -23,23 +33,66 @@
 
 			UIView *avatarView = [[UIView alloc] initWithFrame:self.imageView.bounds];
 			avatarView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-			avatarView.backgroundColor = [UIColor colorWithWhite:0.8f alpha:1];
+			avatarView.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1];
 			avatarView.userInteractionEnabled = NO;
 			avatarView.clipsToBounds = YES;
-			avatarView.layer.cornerRadius = size / 2;
+			avatarView.layer.cornerRadius = IS_MODERN ? size / 2 : 4.f;
 			[self.imageView addSubview:avatarView];
 
-			UILabel *label = [[UILabel alloc] initWithFrame:avatarView.bounds];
-			label.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-			label.font = [UIFont systemFontOfSize:13.f];
-			label.textAlignment = NSTextAlignmentCenter;
-			label.textColor = [UIColor whiteColor];
-			label.text = specifier.properties[@"initials"];
-			[avatarView addSubview:label];
+			if (specifier.properties[@"initials"]) {
+				avatarView.backgroundColor = [UIColor colorWithWhite:0.8f alpha:1];
+
+				UILabel *label = [[UILabel alloc] initWithFrame:avatarView.bounds];
+				label.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+				label.font = [UIFont systemFontOfSize:13.f];
+				label.textAlignment = NSTextAlignmentCenter;
+				label.textColor = [UIColor whiteColor];
+				label.text = specifier.properties[@"initials"];
+				[avatarView addSubview:label];
+			} else {
+				_avatarImageView = [[UIImageView alloc] initWithFrame:avatarView.bounds];
+				_avatarImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+				_avatarImageView.alpha = 0;
+				_avatarImageView.userInteractionEnabled = NO;
+				_avatarImageView.layer.minificationFilter = kCAFilterTrilinear;
+				[avatarView addSubview:_avatarImageView];
+
+				[self loadAvatarIfNeeded];
+			}
 		}
 	}
 
 	return self;
+}
+
+#pragma mark - Avatar
+
+- (UIImage *)avatarImage {
+	return _avatarImageView.image;
+}
+
+- (void)setAvatarImage:(UIImage *)avatarImage {
+	// set the image on the image view
+	_avatarImageView.image = avatarImage;
+
+	// if we haven’t faded in yet
+	if (_avatarImageView.alpha == 0) {
+		// do so now
+		[UIView animateWithDuration:0.15 animations:^{
+			_avatarImageView.alpha = 1;
+		}];
+	}
+}
+
+- (BOOL)_shouldShowAvatar {
+	// if showAvatar is non-nil and YES, use that value. otherwise, if initials
+	// is non-nil, return YES
+	return (self.specifier.properties[@"showAvatar"] && ((NSNumber *)self.specifier.properties[@"showAvatar"]).boolValue) ||
+		self.specifier.properties[@"initials"];
+}
+
+- (void)loadAvatarIfNeeded {
+	// stub for subclasses
 }
 
 @end
--- prefs/HBTwitterCell.m
+++ prefs/HBTwitterCell.m
@@ -1,127 +1,67 @@
 #import "HBTwitterCell.h"
 #import <Preferences/PSSpecifier.h>
-#import <UIKit/UIColor+Private.h>
 #import <UIKit/UIImage+Private.h>
-#import <version.h>
+
+@interface HBLinkTableCell ()
+
+- (BOOL)_shouldShowAvatar;
+
+@end
 
 @interface HBTwitterCell () {
 	NSString *_user;
-	NSString *_initials;
-	UIImage *_defaultImage;
-	UIImage *_highlightedImage;
-
-	UIView *_avatarView;
-	UIImageView *_avatarImageView;
-
-	BOOL _isBig;
 }
 
 @end
 
 @implementation HBTwitterCell
 
++ (NSString *)_urlForUsername:(NSString *)user {
+	user = URL_ENCODE(user);
+
+	// lol, people still copy paste this shitty code
+	if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"aphelion://"]]) {
+		return [@"aphelion://profile/" stringByAppendingString:user];
+	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot://"]]) {
+		return [@"tweetbot:///user_profile/" stringByAppendingString:user];
+	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitterrific://"]]) {
+		return [@"twitterrific:///profile?screen_name=" stringByAppendingString:user];
+	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetings://"]]) {
+		return [@"tweetings:///user?screen_name=" stringByAppendingString:user];
+	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter://"]]) {
+		return [@"twitter://user?screen_name=" stringByAppendingString:user];
+	} else {
+		return [@"https://mobile.twitter.com/" stringByAppendingString:user];
+	}
+}
+
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier {
-	BOOL isBig = specifier.properties[@"big"] && ((NSNumber *)specifier.properties[@"big"]).boolValue;
-	self = [super initWithStyle:isBig ? UITableViewCellStyleSubtitle : UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier specifier:specifier];
+	self = [super initWithStyle:style reuseIdentifier:reuseIdentifier specifier:specifier];
 
 	if (self) {
-		_isBig = isBig;
-		_user = [specifier.properties[@"user"] isEqualToString:@"thekirbylover"] ? @"hbkirb" : [specifier.properties[@"user"] copy];
-		_defaultImage = [UIImage imageNamed:@"twitter" inBundle:globalBundle];
+		((UIImageView *)self.accessoryView).image = [UIImage imageNamed:@"twitter" inBundle:globalBundle];
 
-		if (!IS_MODERN) {
-			_highlightedImage = [UIImage imageNamed:@"twitter_selected" inBundle:globalBundle];
-		}
+		_user = [specifier.properties[@"user"] copy];
+		NSAssert(_user, @"User name not provided");
+
+		specifier.properties[@"url"] = [self.class _urlForUsername:_user];
 
 		self.detailTextLabel.text = [@"@" stringByAppendingString:_user];
-		self.detailTextLabel.textColor = IS_MODERN ? [UIColor colorWithWhite:0.5568627451f alpha:1] : [UIColor tableCellValue1BlueColor];
-		self.selectionStyle = UITableViewCellSelectionStyleBlue;
-		self.accessoryView = [[UIImageView alloc] initWithImage:_defaultImage];
-
-		if (!specifier.properties[@"showAvatar"] || ((NSNumber *)specifier.properties[@"showAvatar"]).boolValue) {
-			CGFloat size = _isBig ? 38.f : 29.f;
-
-			UIGraphicsBeginImageContextWithOptions(CGSizeMake(size, size), NO, [UIScreen mainScreen].scale);
-			specifier.properties[@"iconImage"] = UIGraphicsGetImageFromCurrentImageContext();
-			UIGraphicsEndImageContext();
-
-			_avatarView = [[UIView alloc] initWithFrame:self.imageView.bounds];
-			_avatarView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-			_avatarView.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1];
-			_avatarView.userInteractionEnabled = NO;
-			_avatarView.clipsToBounds = YES;
-			_avatarView.layer.cornerRadius = IS_MODERN ? size / 2 : 4.f;
-			[self.imageView addSubview:_avatarView];
-
-			if (specifier.properties[@"initials"]) {
-				_avatarView.backgroundColor = [UIColor colorWithWhite:0.8f alpha:1];
-
-				UILabel *label = [[UILabel alloc] initWithFrame:_avatarView.bounds];
-				label.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-				label.font = [UIFont systemFontOfSize:13.f];
-				label.textAlignment = NSTextAlignmentCenter;
-				label.textColor = [UIColor whiteColor];
-				label.text = specifier.properties[@"initials"];
-				[_avatarView addSubview:label];
-			} else {
-				_avatarImageView = [[UIImageView alloc] initWithFrame:_avatarView.bounds];
-				_avatarImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-				_avatarImageView.alpha = 0;
-				_avatarImageView.userInteractionEnabled = NO;
-				_avatarImageView.layer.minificationFilter = kCAFilterTrilinear;
-				[_avatarView addSubview:_avatarImageView];
-
-				[self loadAvatarIfNeeded];
-			}
-		}
 	}
 
 	return self;
 }
 
-- (void)setSelectionStyle:(UITableViewCellSelectionStyle)style {
-	[super setSelectionStyle:UITableViewCellSelectionStyleBlue];
-}
-
-- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
-	[super setHighlighted:highlighted animated:animated];
-
-	if (!IS_MODERN) {
-		((UIImageView *)self.accessoryView).image = highlighted ? _highlightedImage : _defaultImage;
-	}
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-	if (!selected) {
-		[super setSelected:selected animated:animated];
-		return;
-	}
-
-	NSString *user = URL_ENCODE(_user);
-	NSURL *url = nil;
-
-	// lol, people still copy paste this shitty code
-	if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"aphelion://"]]) {
-		url = [NSURL URLWithString:[@"aphelion://profile/" stringByAppendingString:user]];
-	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot://"]]) {
-		url = [NSURL URLWithString:[@"tweetbot:///user_profile/" stringByAppendingString:user]];
-	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitterrific://"]]) {
-		url = [NSURL URLWithString:[@"twitterrific:///profile?screen_name=" stringByAppendingString:user]];
-	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetings://"]]) {
-		url = [NSURL URLWithString:[@"tweetings:///user?screen_name=" stringByAppendingString:user]];
-	} else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter://"]]) {
-		url = [NSURL URLWithString:[@"twitter://user?screen_name=" stringByAppendingString:user]];
-	} else {
-		url = [NSURL URLWithString:[@"https://mobile.twitter.com/" stringByAppendingString:user]];
-	}
-
-	[[UIApplication sharedApplication] openURL:url];
-}
-
 #pragma mark - Avatar
 
+- (BOOL)_shouldShowAvatar {
+	// HBLinkTableCell doesn’t want avatars by default, but we do. override its
+	// check method so that if showAvatar is unset, we return YES
+	return self.specifier.properties[@"showAvatar"] ? [super _shouldShowAvatar] : YES;
+}
+
 - (void)loadAvatarIfNeeded {
-	if (!_avatarView || _avatarImageView.image) {
+	if (self.avatarImage) {
 		return;
 	}
 
@@ -134,12 +74,10 @@
 			return;
 		}
 
-		dispatch_async(dispatch_get_main_queue(), ^{
-			_avatarImageView.image = [UIImage imageWithData:data];
+		UIImage *image = [UIImage imageWithData:data];
 
-			[UIView animateWithDuration:0.15 animations:^{
-				_avatarImageView.alpha = 1;
-			}];
+		dispatch_async(dispatch_get_main_queue(), ^{
+			self.avatarImage = image;
 		});
 	});
 }
