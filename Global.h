f02f16ab3f1c39b3f0f1417eae467b9c6bd5dc11
Adam Demasi <adam@hbang.com.au>
2017-04-17 19:20:57 +0930

    [global] i totally borked this sorry

--- Global.h
+++ Global.h
@@ -4,11 +4,10 @@
 
 // percent encoding macro. iOS 9 of course made this more interesting, finally
 // adding an actual api for this, but we support way older than that here…
-#define URL_ENCODE(string) ([NSString instancesRespondToSelector:@selector(stringByAddingPercentEncodingWithAllowedCharacters:)
-] \
-        ? [(string) stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]] \
-        : (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)(string), NULL, CFSTR(":/=,!$& '()*+;[]@#?"), kCFStringEncodingUTF8)))
+#define URL_ENCODE(string) ([NSString instancesRespondToSelector:@selector(stringByAddingPercentEncodingWithAllowedCharacters:)] \
+	? [(string) stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]] \
+	: (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)(string), NULL, CFSTR(":/=,!$& '()*+;[]@#?"), kCFStringEncodingUTF8)))
+
 #pragma mark - Variables
 
 extern NSBundle *globalBundle;
-
