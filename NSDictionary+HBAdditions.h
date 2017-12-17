95b96aa7914946208da88bf5c82b8c3915e4afef
Adam Demasi <adam@hbang.com.au>
2017-12-17 23:37:03 +1030

    [NSDictionary] discuss the fallback implementation in the docs

--- NSDictionary+HBAdditions.h
+++ NSDictionary+HBAdditions.h
@@ -9,7 +9,9 @@ NS_ASSUME_NONNULL_BEGIN
  * Constructs and returns an NSString object that is the result of joining the dictionary keys and
  * values into an HTTP query string.
  *
- * 
+ * On iOS 8.0 and newer, this uses the built in NSURLComponents functionality to deserialize the
+ * query string. On earlier versions, uses an approximation implemented within Cephei. This
+ * implementation is simplistic and does not handle edge cases that NSURLComponents does support.
  *
  * @returns An NSString containing an HTTP query string.
  */
