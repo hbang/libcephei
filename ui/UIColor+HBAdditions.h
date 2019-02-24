e43a22c9ddfc80f8ad5d9b4392d3c040d7542694
Adam Demasi <adam@hbang.com.au>
2019-02-24 22:34:58 +1030

    [ui] add more options for hex values

--- ui/UIColor+HBAdditions.h
+++ ui/UIColor+HBAdditions.h
@@ -8,19 +8,23 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// Creates and returns a color object using data from the specified object.
 ///
-/// The value is expected to either be an array of 3 or 4 integer RGB or RGBA color components
-/// (respectively), with values between 0 and 255, or a CSS hexadecimal color code string.
+/// The value is expected to be one of the types specified in hb_initWithPropertyListValue:.
 ///
 /// @param value The object to retrieve data from. See the discussion for the supported object
 /// types.
 /// @returns The color object. The color information represented by this object is in the device RGB
 /// colorspace.
+/// @see hb_initWithPropertyListValue:
 + (instancetype)hb_colorWithPropertyListValue:(id)value;
 
 /// Initializes and returns a color object using data from the specified object.
 ///
-/// The value is expected to either be an array of 3 or 4 integer RGB or RGBA color components
-/// (respectively), with values between 0 and 255, or a CSS hexadecimal color code string.
+/// The value is expected to be one of:
+///
+/// * An array of 3 or 4 integer RGB or RGBA color components, with values between 0 and 255 (e.g.
+///   `@[ 218, 192, 222 ]`)
+/// * A CSS-style hex string, with an optional alpha component (e.g. `#DAC0DE` or `#DACODE55`)
+/// * A short CSS-style hex string, with an optional alpha component (e.g. `#DC0` or `#DC05`)
 ///
 /// @param value The object to retrieve data from. See the discussion for the supported object
 /// types.
--- ui/UIColor+HBAdditions.m
+++ ui/UIColor+HBAdditions.m
@@ -1,4 +1,16 @@
 #import "UIColor+HBAdditions.h"
+#import <_Prefix/IOSWebKitCompatHacks.h>
+#import <WebKit/WebView.h>
+#import <WebKit/WebFrame.h>
+#import <WebKit/DOMDocument.h>
+#import <WebKit/DOMHTMLDivElement.h>
+#import <WebKit/DOMCSSStyleDeclaration.h>
+#import <WebKit/DOMCSSPrimitiveValue.h>
+#import <WebKit/DOMRGBColor.h>
+
+@interface DOMCSSStyleDeclaration ()
+@property (nonatomic, strong) UIColor *color;
+@end
 
 @implementation UIColor (HBAdditions)
 
@@ -11,17 +23,75 @@
 		return nil;
 	} else if ([value isKindOfClass:NSArray.class] && ((NSArray *)value).count == 3) {
 		NSArray *array = value;
-		return [UIColor colorWithRed:((NSNumber *)array[0]).integerValue / 255.f green:((NSNumber *)array[1]).integerValue / 255.f blue:((NSNumber *)array[2]).integerValue / 255.f alpha:1];
-	} else if ([value isKindOfClass:NSString.class] && [((NSString *)value) hasPrefix:@"#"] && ((NSString *)value).length == 7) {
-		unsigned int hexInteger = 0;
-		NSScanner *scanner = [NSScanner scannerWithString:value];
-		scanner.charactersToBeSkipped = [NSCharacterSet characterSetWithCharactersInString:@"#"];
-		[scanner scanHexInt:&hexInteger];
+		return [self initWithRed:((NSNumber *)array[0]).integerValue / 255.f
+		                   green:((NSNumber *)array[1]).integerValue / 255.f
+		                    blue:((NSNumber *)array[2]).integerValue / 255.f
+		                   alpha:1];
+	} else if ([value isKindOfClass:NSString.class]) {
+		NSString *string = value;
+		if ([string hasPrefix:@"#"] && (string.length == 7 || string.length == 8 || string.length == 4 || string.length == 5)) {
+			if (string.length == 4 || string.length == 5) {
+				NSString *r = [string substringWithRange:NSMakeRange(1, 1)];
+				NSString *g = [string substringWithRange:NSMakeRange(2, 1)];
+				NSString *b = [string substringWithRange:NSMakeRange(3, 1)];
+				NSString *a = string.length == 5 ? [string substringWithRange:NSMakeRange(4, 1)] : @"FF";
+				string = [NSString stringWithFormat:@"#%1$@%1$@%2$@%2$@%3$@%3$@%4$@%4$@", r, g, b, a];
+			}
 
-		return [UIColor colorWithRed:((hexInteger & 0xFF0000) >> 16) / 255.f green:((hexInteger & 0xFF00) >> 8) / 255.f blue:(hexInteger & 0xFF) / 255.f alpha:1];
-	} else {
-		return nil;
+			unsigned int hex = 0;
+			NSScanner *scanner = [NSScanner scannerWithString:string];
+			scanner.charactersToBeSkipped = [NSCharacterSet characterSetWithCharactersInString:@"#"];
+			[scanner scanHexInt:&hex];
+
+			if (string.length == 8) {
+				return [self initWithRed:((hex & 0xFF000000) >> 24) / 255.f
+				                   green:((hex & 0x00FF0000) >> 16) / 255.f
+				                    blue:((hex & 0x0000FF00) >> 8)  / 255.f
+				                   alpha:((hex & 0x000000FF) >> 1)  / 255.f];
+			} else {
+				return [self initWithRed:((hex & 0xFF0000) >> 16) / 255.f
+				                   green:((hex & 0x00FF00) >> 8)  / 255.f
+				                    blue:((hex & 0x0000FF) >> 1)  / 255.f
+				                   alpha:1];
+			}
+		} else if ([string rangeOfString:@"("].location != NSNotFound) {
+			return [self _hb_initWithCSSValue:value];
+		}
 	}
+
+	return nil;
+}
+
+- (instancetype)_hb_initWithCSSValue:(id)value {
+	static dispatch_once_t onceToken;
+	static WebView *webView;
+	static DOMHTMLDivElement *div;
+	dispatch_once(&onceToken, ^{
+		webView = [[WebView alloc] init];
+		DOMDocument *document = webView.mainFrame.DOMDocument;
+		div = (DOMHTMLDivElement *)[document createElement:@"div"];
+		[document.body appendChild:div];
+	});
+
+	div.style.color = value;
+
+	DOMDocument *document = webView.mainFrame.DOMDocument;
+	DOMCSSStyleDeclaration *computedStyle = [document getComputedStyle:div pseudoElement:nil];
+	DOMCSSPrimitiveValue *cssValue = (DOMCSSPrimitiveValue *)[computedStyle getPropertyCSSValue:@"color"];
+
+	if (cssValue) {
+		DOMRGBColor *color = cssValue.getRGBColorValue;
+		CGFloat components[4] = {
+			[color.red getFloatValue:DOM_CSS_PRIMITIVE_VALUE] / 255.f,
+			[color.green getFloatValue:DOM_CSS_PRIMITIVE_VALUE] / 255.f,
+			[color.blue getFloatValue:DOM_CSS_PRIMITIVE_VALUE] / 255.f,
+			[color.alpha getFloatValue:DOM_CSS_PRIMITIVE_VALUE]
+		};
+
+		return [self initWithRed:components[0] green:components[1] blue:components[2] alpha:components[3]];
+	}
+
+	return nil;
 }
 
 @end
