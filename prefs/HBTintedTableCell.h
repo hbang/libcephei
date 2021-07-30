652c782d2170701dae7116b755c050096da4ab37
Adam Demasi <adam@hbang.com.au>
2021-07-30 22:56:35 +0930

    [HBTintedTableCell] Throw in custom tintColor feature

--- prefs/HBTintedTableCell.h
+++ prefs/HBTintedTableCell.h
@@ -3,6 +3,18 @@
 /// The HBTintedTableCell class in CepheiPrefs ensures that a tint set with `HBAppearanceSettings`
 /// will also be applied to the title label of a of a cell intended to be used as a button.
 ///
+/// ### Specifier Parameters
+/// HBListController extends specifiers with the following parameters:
+///
+/// <table>
+/// <tr>
+/// <th>tintColor</th> <td>Optional. The color to use for the label of the cell. The default is no
+/// value, which means the tint color will be inherited from the
+/// `-[HBAppearanceSettings tintColor]`; if neither value is set, the default iOS blue tint color is
+/// used.</td>
+/// </tr>
+/// </table>
+///
 /// ### Example Usage
 /// ```xml
 /// <dict>
@@ -12,6 +24,18 @@
 /// 	<string>HBTintedTableCell</string>
 /// 	<key>label</key>
 /// 	<string>Do Something</string>
+/// </dict>
+///
+/// <!-- Or with a custom tint color: -->
+/// <dict>
+/// 	<key>cell</key>
+/// 	<string>PSButtonCell</string>
+/// 	<key>cellClass</key>
+/// 	<string>HBTintedTableCell</string>
+/// 	<key>label</key>
+/// 	<string>Do Something</string>
+/// 	<key>tintColor</key>
+/// 	<string>#33b5e5</string>
 /// </dict>
 /// ```
 
--- prefs/HBTintedTableCell.m
+++ prefs/HBTintedTableCell.m
@@ -1,6 +1,17 @@
 #import "HBTintedTableCell.h"
+#import <Preferences/PSSpecifier.h>
+#import "../ui/UIColor+HBAdditions.h"
 
 @implementation HBTintedTableCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier {
+	self = [super initWithStyle:style reuseIdentifier:reuseIdentifier specifier:specifier];
+	if (self) {
+		id tintColorString = specifier.properties[@"tintColor"];
+		self.tintColor = [UIColor hb_colorWithPropertyListValue:tintColorString];
+	}
+	return self;
+}
 
 - (void)tintColorDidChange {
 	[super tintColorDidChange];
