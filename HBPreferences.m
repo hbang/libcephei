4072f592bba18a8424cbaae6e456a80f5281eff7
Adam Demasi <adam@hbang.com.au>
2023-05-10 12:49:25 +0930

    [HBPreferences] Not sure what made me think this wasn’t needed…
    This reverts commit 7d6e785500413a8a8a4a5a40550f5bee61f99d93.

--- HBPreferences.m
+++ HBPreferences.m
@@ -5,26 +5,52 @@
 // Ensure private symbols don’t get included if we’re in embedded mode. Any empty code paths will be
 // optimised out by the compiler.
 #if !CEPHEI_EMBEDDED
+#import <dlfcn.h>
 #import <sandbox.h>
+
+#define kCFPreferencesNoContainer CFSTR("kCFPreferencesNoContainer")
+
+typedef CFPropertyListRef (*_CFPreferencesCopyValueWithContainerType)(CFStringRef key, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName, CFStringRef containerPath);
+typedef void (*_CFPreferencesSetValueWithContainerType)(CFStringRef key, CFPropertyListRef value, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName, CFStringRef containerPath);
+typedef Boolean (*_CFPreferencesSynchronizeWithContainerType)(CFStringRef applicationID, CFStringRef userName, CFStringRef hostName, CFStringRef containerPath);
+typedef CFArrayRef (*_CFPreferencesCopyKeyListWithContainerType)(CFStringRef applicationID, CFStringRef userName, CFStringRef hostName, CFStringRef containerPath);
+typedef CFDictionaryRef (*_CFPreferencesCopyMultipleWithContainerType)(CFArrayRef keysToFetch, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName, CFStringRef containerPath);
+
+static _CFPreferencesCopyValueWithContainerType _CFPreferencesCopyValueWithContainer;
+static _CFPreferencesSetValueWithContainerType _CFPreferencesSetValueWithContainer;
+static _CFPreferencesSynchronizeWithContainerType _CFPreferencesSynchronizeWithContainer;
+static _CFPreferencesCopyKeyListWithContainerType _CFPreferencesCopyKeyListWithContainer;
+static _CFPreferencesCopyMultipleWithContainerType _CFPreferencesCopyMultipleWithContainer;
+
 static BOOL isSystemApp;
 #endif
 
+NSString *const HBPreferencesNotMobileException = @"HBPreferencesNotMobileException";
+
 #pragma mark - Class implementation
 
-@implementation HBPreferences
+@implementation HBPreferences {
+	CFStringRef _container;
+}
 
 #pragma mark - Initialization
 
-#if !CEPHEI_EMBEDDED
 + (void)initialize {
 	[super initialize];
 
+#if !CEPHEI_EMBEDDED
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
+		_CFPreferencesCopyValueWithContainer = (_CFPreferencesCopyValueWithContainerType)dlsym(RTLD_DEFAULT, "_CFPreferencesCopyValueWithContainer");
+		_CFPreferencesSetValueWithContainer = (_CFPreferencesSetValueWithContainerType)dlsym(RTLD_DEFAULT, "_CFPreferencesSetValueWithContainer");
+		_CFPreferencesSynchronizeWithContainer = (_CFPreferencesSynchronizeWithContainerType)dlsym(RTLD_DEFAULT, "_CFPreferencesSynchronizeWithContainer");
+		_CFPreferencesCopyKeyListWithContainer = (_CFPreferencesCopyKeyListWithContainerType)dlsym(RTLD_DEFAULT, "_CFPreferencesCopyKeyListWithContainer");
+		_CFPreferencesCopyMultipleWithContainer = (_CFPreferencesCopyMultipleWithContainerType)dlsym(RTLD_DEFAULT, "_CFPreferencesCopyMultipleWithContainer");
+
 		isSystemApp = IS_SYSTEM_APP;
 	});
-}
 #endif
+}
 
 - (instancetype)initWithIdentifier:(NSString *)identifier {
 	NSParameterAssert(identifier);
@@ -41,6 +67,18 @@ static BOOL isSystemApp;
 	// of the class that works around this by doing IPC with our preferences server.
 	if (isSystemApp || sandbox_check(getpid(), "user-preference-read", SANDBOX_FILTER_PREFERENCE_DOMAIN | SANDBOX_CHECK_NO_REPORT, identifier) == KERN_SUCCESS) {
 		self = [super initWithIdentifier:identifier];
+
+		// iOS 8 and newer don’t fall back to the user’s home directory if the identifier isn’t found
+		// within the container’s directory. We also assume no container is in use if the process is
+		// running as root.
+		// A nil container indicates to use the current container. kCFPreferencesNoContainer forces the
+		// user’s home directory to be used. We assume that if the identifier starts with the app bundle
+		// id, it probably wants its own preferences inside its container.
+		// TODO: Is there a better way to guess this? Should we not guess at all except for the exact
+		// main bundle id?
+		if (getuid() != 0 && ![[[NSBundle mainBundle].bundleIdentifier stringByAppendingString:@"."] hasPrefix:[identifier stringByAppendingString:@"."]] && ![[NSBundle mainBundle].bundleIdentifier isEqualToString:@"ws.hbang.Terminal"]) {
+			_container = kCFPreferencesNoContainer;
+		}
 	} else {
 		self = (HBPreferences *)[[HBPreferencesIPC alloc] initWithIdentifier:identifier];
 	}
@@ -52,33 +90,76 @@ static BOOL isSystemApp;
 #pragma mark - Reloading
 
 - (BOOL)synchronize {
-	return CFPreferencesSynchronize((__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
+	if (_container) {
+#if !CEPHEI_EMBEDDED
+		return _CFPreferencesSynchronizeWithContainer((__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost, _container);
+#endif
+	} else {
+		return CFPreferencesSynchronize((__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
+	}
 }
 
 #pragma mark - Dictionary representation
 
 - (NSDictionary <NSString *, id> *)dictionaryRepresentation {
-	CFArrayRef allKeys = CFPreferencesCopyKeyList((__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
-	if (!allKeys) {
-		return @{};
+	CFDictionaryRef result;
+
+	if (_container) {
+#if !CEPHEI_EMBEDDED
+		CFArrayRef allKeys = _CFPreferencesCopyKeyListWithContainer((__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost, _container);
+
+		if (!allKeys) {
+			return @{};
+		}
+
+		result = _CFPreferencesCopyMultipleWithContainer(allKeys, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost, _container);
+		CFRelease(allKeys);
+#endif
+	} else {
+		CFArrayRef allKeys = CFPreferencesCopyKeyList((__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
+
+		if (!allKeys) {
+			return @{};
+		}
+
+		result = CFPreferencesCopyMultiple(allKeys, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
+		CFRelease(allKeys);
 	}
-	CFDictionaryRef result = CFPreferencesCopyMultiple(allKeys, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
-	CFRelease(allKeys);
+
 	return (__bridge_transfer NSDictionary *)result;
 }
 
 #pragma mark - Getters
 
 - (id)_objectForKey:(NSString *)key {
-	id value = CFBridgingRelease(CFPreferencesCopyValue((__bridge CFStringRef)key, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost));
-	[self _storeValue:value forKey:key];
-	return value;
+	CFTypeRef value;
+
+	if (_container) {
+#if !CEPHEI_EMBEDDED
+		value = _CFPreferencesCopyValueWithContainer((__bridge CFStringRef)key, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost, _container);
+#endif
+	} else {
+		value = CFPreferencesCopyValue((__bridge CFStringRef)key, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
+	}
+
+	id objcValue = CFBridgingRelease(value);
+
+	[self _storeValue:objcValue forKey:key];
+
+	return objcValue;
 }
 
 #pragma mark - Setters
 
 - (void)_setObject:(id)value forKey:(NSString *)key {
-	CFPreferencesSetValue((__bridge CFStringRef)key, (__bridge CFPropertyListRef)value, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
+	if (_container) {
+#if !CEPHEI_EMBEDDED
+		_CFPreferencesSetValueWithContainer((__bridge CFStringRef)key, (__bridge CFPropertyListRef)value, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost, _container);
+#endif
+	} else {
+		CFPreferencesSetValue((__bridge CFStringRef)key, (__bridge CFPropertyListRef)value, (__bridge CFStringRef)self.identifier, CFSTR("mobile"), kCFPreferencesAnyHost);
+	}
+
 	[self _storeValue:value forKey:key];
 }
 
